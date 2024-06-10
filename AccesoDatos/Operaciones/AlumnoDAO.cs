dfb3d40c89406ecea0e04dfe708d12de23cb2169
Nelson Ysain Ortiz Leal <nelsonortizleal@gmail.com>
2024-06-10 00:12:57 -0600

    Add: Clase para registrar las operaciones permitidas en la tabla Alumno

---
+++ AccesoDatos/Operaciones/AlumnoDAO.cs
@@ -0,0 +1,31 @@
+﻿using AccesoDatos.Context;
+using AccesoDatos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos.Operaciones
+{
+    public class AlumnoDAO
+    {
+        public List<Alumno> SeleccionarTodos()
+        {
+            using (ProyectoContext contexto = new ProyectoContext())
+            {
+                var alumnos = contexto.Alumnos.ToList<Alumno>();
+                return alumnos;
+            }
+        }
+
+        public Alumno SeleccionarAlumno(int id)
+        {
+            using (ProyectoContext contexto = new ProyectoContext())
+            {
+                var alumno = contexto.Alumnos.Find(id);
+                return alumno;
+            }
+        }
+    }
+}
