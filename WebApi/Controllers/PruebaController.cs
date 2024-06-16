ea9fc64bc9801bc90d542324fb8ec0631c2e62f5
Nelson Ysain Ortiz Leal <nelsonortizleal@gmail.com>
2024-06-16 00:27:37 -0600

    Add: Endpoint de 'Hola mundo'

---
+++ WebApi/Controllers/PruebaController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PruebaController : ControllerBase
+    {
+        // Creamos nuestros endpoints 
+        [HttpGet(Name = "prueba")]
+        public string pruebaApi()
+        {
+            return "Esto es una prueba de mi API.";
+        }
+    }
+}
