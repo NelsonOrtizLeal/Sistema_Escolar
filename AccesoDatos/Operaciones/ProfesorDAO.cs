b4ceb77e45501c9f3e2bc0e5b326fbb0d6a3a46a
Nelson Ysain Ortiz Leal <nelsonortizleal@gmail.com>
2024-06-17 00:39:43 -0600

    Add: Controlador Profesor y endpoint para validar el login del usuario

---
+++ AccesoDatos/Operaciones/ProfesorDAO.cs
@@ -0,0 +1,18 @@
+﻿using AccesoDatos.Context;
+using AccesoDatos.Models;
+
+namespace AccesoDatos.Operaciones
+{
+    public class ProfesorDAO
+    {
+        public Profesor? login(string username, string password)
+        {
+            using (ProyectoContext contexto = new ProyectoContext())
+            {
+                var profesor = contexto.Profesors.Where(p => p.Usuario.Equals(username) && p.Pass.Equals(password)).FirstOrDefault();
+
+                return profesor;
+            }
+        }
+    }
+}
---
+++ WebApi/Controllers/ProfesorController.cs
@@ -0,0 +1,27 @@
+﻿using AccesoDatos.Models;
+using AccesoDatos.Operaciones;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfesorController : ControllerBase
+    {
+        // ProfesorDAO de capa DAL
+        public ProfesorDAO profesorDAO = new ProfesorDAO();
+
+        [HttpPost("autenticacion")]
+        public string login([FromBody] Profesor prof)
+        {
+            var profesor = profesorDAO.login(prof.Usuario, prof.Pass);
+
+            if (profesor == null)
+            {
+                return "";
+            }
+
+            return profesor.Usuario;
+        }
+    }
+}
