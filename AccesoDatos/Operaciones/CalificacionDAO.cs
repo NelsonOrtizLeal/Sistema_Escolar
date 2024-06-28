8b8a9a7d755d631928664313d6e830d8248a0479
Nelson Ysain Ortiz Leal <nelsonortizleal@gmail.com>
2024-06-28 01:33:59 -0600

    Add: Endpoint para obtener las calificaciones de un alumno

---
+++ AccesoDatos/Operaciones/CalificacionDAO.cs
@@ -0,0 +1,26 @@
+﻿using AccesoDatos.Context;
+using AccesoDatos.Models;
+
+namespace AccesoDatos.Operaciones
+{
+    public class CalificacionDAO
+    {
+        public List<Calificacion> CalificacionesMatricula(int id_matricula)
+        {
+            try
+            {
+                using (ProyectoContext contexto = new ProyectoContext())
+                {
+                    var calificaciones = contexto.Calificacions.Where(c => c.MatriculaId.Equals(id_matricula)).ToList();
+                    return calificaciones;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+
+                return new List<Calificacion>();
+            }
+        }
+    }
+}
---
+++ WebApi/Controllers/CalificacionController.cs
@@ -0,0 +1,19 @@
+﻿using AccesoDatos.Models;
+using AccesoDatos.Operaciones;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalificacionController : ControllerBase
+    {
+        CalificacionDAO calificacionDAO = new CalificacionDAO();
+
+        [HttpGet("calificaciones")]
+        public List<Calificacion> CalificacionesMatricula(int id_matricula)
+        {
+            return calificacionDAO.CalificacionesMatricula(id_matricula);
+        }
+    }
+}
